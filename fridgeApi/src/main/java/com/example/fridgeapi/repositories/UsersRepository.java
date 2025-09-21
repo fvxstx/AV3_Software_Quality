0158eff0834a26f2006a408e73d59e5372435609
CaueLuz20 <caue.luz14@gmail.com>
2025-09-21 16:07:52 -0300

    juntando

--- fridgeApi/src/main/java/com/example/fridgeapi/repositories/UsersRepository.java
+++ fridgeApi/src/main/java/com/example/fridgeapi/repositories/UsersRepository.java
@@ -7,9 +7,5 @@ import java.math.BigInteger;
 import java.util.Optional;
 
 public interface UsersRepository extends JpaRepository<Users, BigInteger> {
-<<<<<<< .merge_file_xuwMKo
     Optional<Users> findByEmail(String email);
-=======
-    
->>>>>>> .merge_file_3L5Xnu
 }
