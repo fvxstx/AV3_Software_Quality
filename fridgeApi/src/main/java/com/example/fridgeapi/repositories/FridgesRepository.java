1608c30889f6db0f2bc3c5d7e11a92d36c68bac3
Diogo <dooss2085@gmail.com>
2025-11-13 05:33:08 -0300

    feat: creates integration tests for fridges

--- fridgeApi/src/main/java/com/example/fridgeapi/repositories/FridgesRepository.java
+++ fridgeApi/src/main/java/com/example/fridgeapi/repositories/FridgesRepository.java
@@ -4,6 +4,5 @@ import com.example.fridgeapi.models.Fridges;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FridgesRepository extends JpaRepository<Fridges, Long> {
-    void save(Long fridgeId);
 }
 
